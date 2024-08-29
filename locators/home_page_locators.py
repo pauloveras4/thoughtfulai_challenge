96da6d7dbd95a2ceec12d84bf59664a289c532e3
Paulo Veras <pauloveraa4@gmail.com>
2024-08-29 20:00:00 -0300

    Changing to Extended Selenium.

---
+++ locators/home_page_locators.py
@@ -0,0 +1,10 @@
+class HomePageLocators(object):
+    """
+    A class for storing home page locators. All locators related to
+    the home page will be stored in here.
+    """
+
+    HOME_PAGE_SEARCH_BAR_ICON = "//div[contains(@class, 'search-bar')]"
+
+    HOME_PAGE_SEARCH_BAR_INPUT = "//*[contains(@class, 'search-bar__bar-container')]/input"
+    
--- tasks.py
+++ tasks.py
@@ -1,6 +1,9 @@
 from robocorp.tasks import task
+from lib.extended_selenium import ExtendedSelenium
 
 @task
 def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+    selenium = ExtendedSelenium()
+    selenium.open_site("https://www.reuters.com")
+    selenium.looking_at_element('//*[@id="fusion-app"]/header/div/div/div/div/div[3]/div[1]')
+    print("Feito")
