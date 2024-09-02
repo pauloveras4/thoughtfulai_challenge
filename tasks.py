6c46f6cb82c37d8bf256fb86885b7cc4ac707699
Paulo Veras <pauloveraa4@gmail.com>
2024-09-01 21:54:24 -0300

    Trying to fix Selenium Browser is Not Open error on Control Room.

--- tasks.py
+++ tasks.py
@@ -15,20 +15,28 @@ logging.basicConfig(filename='output/RPA_Reuters.log', filemode='a', level=loggi
 
 @task
 def minimal_task():
+    """ items = WorkItems()
+    variables = items.get_work_item_variables()
+    search_query = variables['search_query']
+    section = variables['section']
+    months_index = variables['months_index']
+    
+ """    
     search_query = "cookies"
     section = "All"
-    months_index = 0
+    months_index = "0"
+    months_index = int(months_index)
     
     selenium = ExtendedSelenium()
     base_page = BasePage(selenium)
     base_page.access_reuters_page()
     
-    home_page = HomePage(selenium)
+    home_page = HomePage(base_page.selenium)
     home_page.click_search_bar_icon()
     home_page.enter_search_query(search_query)
     home_page.click_search_bar_icon()
     
-    results_page = ResultsPage(selenium, months_index, search_query)
+    results_page = ResultsPage(home_page.selenium, months_index, search_query)
     results_page.select_section_filter_from_dropdown(section)
     results_page.select_sort_by_order_from_dropdown("Newest")
     results_array = results_page.iter_through_all_news_until_date()
